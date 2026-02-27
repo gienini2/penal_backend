ea4506de563e394b688932c37fa1d47d0000b659
gienini2 <gienini2@gmail.com>
2026-02-27 10:27:32 +0100

    Create engine.py

---
+++ modules/contra_orden_publico/engine.py
@@ -0,0 +1,10 @@
+from .extractor import extraer_vector
+from .similarity import comparar_patrimonio
+
+CATALOGO_PATH = "modules/contra_orden_publico/catalogo"
+UMBRAL = 0.50
+
+def run(texto):
+    vector = extraer_vector(texto)
+    ranking = comparar_patrimonio(vector, CATALOGO_PATH, UMBRAL)
+    return ranking
