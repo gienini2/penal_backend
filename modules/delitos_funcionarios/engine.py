4bc4037a867344231c2f5ff649f898d726e8be43
gienini2 <gienini2@gmail.com>
2026-02-28 07:24:37 +0100

    Update engine.py

--- modules/delitos_funcionarios/engine.py
+++ modules/delitos_funcionarios/engine.py
@@ -1,10 +1,10 @@
 from .extractor import extraer_vector
-from .similarity import comparar_patrimonio
+from .similarity import comparar_funcion_publica
 
 CATALOGO_PATH = "modules/delitos_funcionarios/catalogo"
 UMBRAL = 0.50
 
 def run(texto):
     vector = extraer_vector(texto)
-    ranking = comparar_patrimonio(vector, CATALOGO_PATH, UMBRAL)
+    ranking = comparar_funcion_publica(vector, CATALOGO_PATH, UMBRAL)
     return ranking
