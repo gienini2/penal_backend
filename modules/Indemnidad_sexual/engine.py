38e9d830170bc6a911bcc0a8a0a466d4aee5a99c
gienini2 <gienini2@gmail.com>
2026-02-28 07:25:38 +0100

    Update engine.py

--- modules/Indemnidad_sexual/engine.py
+++ modules/Indemnidad_sexual/engine.py
@@ -1,10 +1,10 @@
 from .extractor import extraer_vector
-from .similarity import comparar_patrimonio
+from .similarity import comparar_sexual
 
 CATALOGO_PATH = "modules/Indemnidad_sexual/catalogo"
 UMBRAL = 0.50
 
 def run(texto):
     vector = extraer_vector(texto)
-    ranking = comparar_patrimonio(vector, CATALOGO_PATH, UMBRAL)
+    ranking = comparar_sexual(vector, CATALOGO_PATH, UMBRAL)
     return ranking
