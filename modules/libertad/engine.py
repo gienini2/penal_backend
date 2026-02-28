a61af6dbd30ff5f7617036a956d082ed175d3e9d
gienini2 <gienini2@gmail.com>
2026-02-28 07:23:17 +0100

    Update engine.py

--- modules/libertad/engine.py
+++ modules/libertad/engine.py
@@ -1,10 +1,10 @@
 from .extractor import extraer_vector
-from .similarity import comparar_patrimonio
+from .similarity import comparar_libertad
 
-CATALOGO_PATH = "modules/Libertad/catalogo"
+CATALOGO_PATH = "modules/libertad/catalogo"
 UMBRAL = 0.50
 
 def run(texto):
     vector = extraer_vector(texto)
-    ranking = comparar_patrimonio(vector, CATALOGO_PATH, UMBRAL)
+    ranking = comparar_libertad(vector, CATALOGO_PATH, UMBRAL)
     return ranking
