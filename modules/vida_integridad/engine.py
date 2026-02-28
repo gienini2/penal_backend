3fae38f3927bf4c7808e30a7ee343c756bcec28d
gienini2 <gienini2@gmail.com>
2026-02-28 07:09:31 +0100

    Update engine.py

--- modules/vida_integridad/engine.py
+++ modules/vida_integridad/engine.py
@@ -1,10 +1,10 @@
 from .extractor import extraer_vector
 from .similarity import comparar_patrimonio
 
-CATALOGO_PATH = "modules/Vida_e_integridad_fisica/catalogo"
+CATALOGO_PATH = "modules/vida_integridad/catalogo"
 UMBRAL = 0.50
 
 def run(texto):
     vector = extraer_vector(texto)
-    ranking = comparar_patrimonio(vector, CATALOGO_PATH, UMBRAL)
+    ranking = comparar_vida_integridad(vector, CATALOGO_PATH, UMBRAL)
     return ranking
